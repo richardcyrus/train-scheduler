d9b89fa5d870604b2c207c68549c189e03d69e16
Richard Cyrus <richard.cyrus@rcyrus.com>
2018-11-04 20:42:02 -0500

    Multiple Enhancements
    * Add support for counting down the minutes away and updating the new train time.
    * Add form validation.
    * Add jQuery.timepicker to normalize the time selection across browsers.

--- README.md
+++ README.md
@@ -4,24 +4,25 @@
 
 * Create an application that will allow the user to add records for train schedules.
 * When adding a train, the administrator must provide the following:
-    - The name of the train.
+    - The starting location of the train.
     - The destination.
     - The time of first departure.
     - The frequency at which the train departs.
-* For the departure time of the first train, that value should be entered in UTC time format.
-* For the frequency between trains, that value is in minutes.
+* For the departure time of the first train, that value should be entered in 24-hour time format (i.e. 18:00).
+* For the frequency between trains is in minutes.
 * Calculate the time of the next arrival of a train.
 * The Next arrival should be displayed as relative to the current time.
-* The schedule time of the trains should ve viewable by multiple users.
+* The schedule time of the trains should be viewable by multiple users.
 
 ## Bonus
 
 * Update the "Minutes Away" and "Next Arrival" every minute.
 * Implement update and remove buttons for each train.
 * Allow users to edit the elements that constitute a train schedule:
-    - The Train's Name
-    - The Destination
-    - The Arrival time
+    - The train's starting point.
+    - The destination.
+    - The departure frequency.
+    - The arrival time.
 
 ## Extra Bonus
 
@@ -30,5 +31,5 @@
 
 # Tools and Libraries
 
-* Use Firebase to make the site multi-user capable.
+* Must use Firebase to make the site multi-user capable.
 * Use Moment.js to simplify the date/time calculations.
--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,12 @@
 (function ($) {
     "use strict";
 
-    const config = {
+    /**
+     * The configuration information, needed to connect with Firebase.
+     *
+     * @type {{apiKey: string, authDomain: string, databaseURL: string, projectId: string, storageBucket: string, messagingSenderId: string}}
+     */
+    const firebaseConfig = {
         apiKey: "AIzaSyAubOqrSPuKuz3ftTDNQCpetHJu_dE7AMk",
         authDomain: "train-scheduler-bf006.firebaseapp.com",
         databaseURL: "https://train-scheduler-bf006.firebaseio.com",
@@ -21,7 +26,20 @@
 
     const TrainScheduler = (function () {
 
-        const form = $('form');
+        /**
+         * The form on the page used to add new train records to the
+         * schedule.
+         *
+         * @type {*|jQuery|HTMLElement}
+         */
+        const form = $('#add-train');
+
+        /**
+         * The body of the table where we will display the new schedule
+         * records.
+         *
+         * @type {*|jQuery|HTMLElement}
+         */
         const table = $('tbody');
 
         /**
@@ -35,6 +53,13 @@
             const minutes = date.getMinutes();
             const seconds = date.getSeconds();
 
+            /**
+             * Determine if the provided number should have a leading
+             * zero added to it.
+             *
+             * @param unit
+             * @returns {string}
+             */
             function padZero(unit) {
                 return (unit < 10) ? `0${unit}` : unit;
             }
@@ -47,37 +72,89 @@
         };
 
         /**
+         * Manage the countdown timer for the Minutes Away column in the
+         * schedule.
+         *
+         * @param minutes
+         * @param displayUpdate
+         * @param runWhenFinished
+         */
+        const timer = function (minutes, displayUpdate = false, runWhenFinished = false) {
+            const now = Date.now();
+            const then = now + (minutes * 60) * 1000;
+
+            const intervalId = setInterval(() => {
+                let timeLeft = Math.round((then - Date.now()) / 1000);
+
+                if (timeLeft <= 0) {
+                    clearInterval(intervalId);
+
+                    if (runWhenFinished) {
+                        runWhenFinished();
+                    }
+
+                    return;
+                }
+
+                if (displayUpdate) {
+                    displayUpdate(timeLeft);
+                }
+            }, 1000);
+        };
+
+        /**
+         * Format the whole number of minutes into 00:00.
+         *
+         * @param minutes
+         * @returns {string}
+         */
+        const formatMinutes = function (minutes) {
+            const seconds = Math.floor(minutes * 60);
+            const remainder = seconds % 60;
+
+            return `${minutes}:${remainder < 10 ? '0' : ''}${remainder}`;
+        };
+
+        /**
          * Determine the time to the next train returning both the
          * total minutes (next train) and the remaining interval
          * (minutes to next).
          *
-         * @param departure
-         * @param interval
+         * @param firstDeparture
+         * @param departureInterval
          * @returns {{nextArrival: *, minutesAway: number}}
          */
-        const getTrainTimes = function (departure, interval) {
-
+        const getTrainTimes = function (firstDeparture, departureInterval) {
             // Get the current date & time.
             let now = moment();
 
             // Convert the departure date, into a date 1 year ago.
             // This ensures for comparison purposes it's older than now().
-            let firstDeparture = moment(departure, moment.HTML5_FMT.TIME).subtract(1, 'years');
+            let startingDeparture = moment(
+                firstDeparture,
+                moment.HTML5_FMT.TIME
+            ).subtract(1, 'years');
 
             // Get the difference in minutes between now, and the
             // first departure time.
-            let differenceMinutes = now.diff(firstDeparture, 'minutes');
+            let differenceMinutes = now.diff(
+                startingDeparture,
+                'minutes'
+            );
 
             // Get the modulus of the minutes difference and the scheduling
             // interval.
-            let intervalRemainder = differenceMinutes % interval;
+            let intervalRemainder = differenceMinutes % departureInterval;
 
             // The time to the next train is the interval, minus the
             // modulus of minutes difference from now and the interval.
-            let minutesToNext = interval - intervalRemainder;
+            let minutesToNext = departureInterval - intervalRemainder;
 
             // The next train will arrive in now + minutes to next.
-            let nextTrain = now.add(minutesToNext, 'minutes');
+            let nextTrain = now.add(
+                minutesToNext,
+                'minutes'
+            );
 
             // Send the important parts to the caller.
             return {
@@ -87,69 +164,212 @@
         };
 
         /**
+         * When the 'Minutes Away' counter reaches 0, get and display
+         * the new 'Next Train' time, and start the 'Minutes Away' counter
+         * again.
+         *
+         * @param data
+         * @param scheduleSelectors
+         */
+        const updateTrainTimeCounters = function (data, scheduleSelectors) {
+            // Get the next times.
+            const times = getTrainTimes(
+                data.firstTrainTime,
+                data.trainInterval
+            );
+
+            // Update with the next time for the train.
+            $(scheduleSelectors.nextTrainTimeSelector).text(
+                times.nextArrival.format('HH:mm A'));
+
+            // Update the 'Minutes Away' display.
+            $(scheduleSelectors.minutesAwaySelector).text(
+                formatMinutes(times.minutesAway));
+
+            // Start the 'Minutes Away' counter.
+            updateTimes(scheduleSelectors, data, times.minutesAway);
+        };
+
+        /**
+         * Start the initial 'Minutes Away' counter when a record is added,
+         * and setup to update the 'Next Train' and 'Minutes Away' when
+         * the initial timer expires.
+         *
+         * @param scheduleSelectors
+         * @param train
+         * @param minutes
+         */
+        const updateTimes = function (scheduleSelectors, train, minutes) {
+            /**
+             * Update the display of the 'Minutes Away' from the results
+             * of the `timer` function.
+             *
+             * @param seconds
+             */
+            const countdown = function (seconds) {
+                const minutes = Math.floor(seconds / 60);
+                const remainder = seconds % 60;
+
+                let time = `${minutes}:${remainder < 10 ? '0' : ''}${remainder}`;
+
+                $(scheduleSelectors.minutesAwaySelector).text(time);
+            };
+
+            // Start the 'Minutes Away' timer.
+            timer(minutes, countdown, () => {
+                return updateTrainTimeCounters(train, scheduleSelectors);
+            });
+        };
+
+        /**
          * Add a row to the 'Current Train Schedule' table.
          *
          * @param data
          */
-        const showTrain = function (data) {
-            const times = getTrainTimes(data.first,  data.interval);
+        const addScheduledTrain = function (data) {
+            // Create a random identifier for the table rows so that we
+            // can accurately update the 'Minutes Away' timer and the
+            // 'Next Train' time.
+            const rowIdentifier = Math.random()
+                .toString(36)
+                .replace(/[^a-z]+/g, '')
+                .substr(0, 5);
 
-            const row = `
-                <tr>
-                    <td>${data.start}</td>
-                    <td>${data.end}</td>
-                    <td>${data.interval}</td>
-                    <td>${times.nextArrival.format('hh:mm A')}</td>
-                    <td>${times.minutesAway}</td>
+            const times = getTrainTimes(
+                data.firstTrainTime,
+                data.trainInterval
+            );
+
+            const trainScheduleSelectors = {
+                minutesAwaySelector: `#${rowIdentifier} > .minutes-away`,
+                nextTrainTimeSelector: `#${rowIdentifier} > .next-train-time`
+            };
+
+            const tableRow = `
+                <tr id="${rowIdentifier}">
+                    <td>${data.startingLocation}</td>
+                    <td>${data.destination}</td>
+                    <td class="text-right">${data.trainInterval}</td>
+                    <td class="text-right pr-2 next-train-time">
+                        ${times.nextArrival.format('HH:mm A')}
+                    </td>
+                    <td class="text-right pr-4 minutes-away">
+                        ${formatMinutes(times.minutesAway)}
+                    </td>
                     <td class="d-flex justify-content-around">
-                        <span class="fas fa-pencil-alt edit"></span>
-                        <span class="fas fa-trash-alt remove"></span>
+                        <button class="btn btn-danger remove" type="button" role="delete">
+                            <span class="fas fa-trash-alt"></span>
+                        </button>
                     </td>
                 </tr>`;
 
-            table.append(row);
+            table.append(tableRow);
+
+            // Start the 'Minutes Away' timer.
+            updateTimes(trainScheduleSelectors, data, times.minutesAway);
         };
 
         /**
          * Register the click and other handlers for the application.
          */
         const registerHandlers = function () {
+            /**
+             * Handle Form validation and submission of user provided
+             * values.
+             */
             form.on('submit', function (e) {
-                e.preventDefault();
+                /**
+                 * Trigger client side form validation.
+                 *
+                 * The checkValidity() call has to occur on a form node
+                 * not on the <form> element itself.
+                 *
+                 * $(this) = [form#add-train.needs-validation]
+                 * $(this)[0] = the full form HTML with all fields.
+                 */
+                if ($(this)[0].checkValidity() === false) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+                form.addClass('was-validated');
 
-                const train = {
-                    'start': null,
-                    'end': null,
-                    'first': null,
-                    'interval': 0
-                };
+                // If the form is valid, process it.
+                if (! e.isDefaultPrevented()) {
+                    // Prevent the default action of the submit button.
+                    e.preventDefault();
 
-                $.each($('input'), function () {
-                    if ($(this).is('input[name="train_name"]')) {
-                        train.start = $(this).val();
-                    } else if ($(this).is('input[name="train_destination"]')) {
-                        train.end = $(this).val();
-                    } else if ($(this).is('input[name="first_train"]')) {
-                        train.first = $(this).val();
-                    } else if ($(this).is('input[name="departure_interval"]')) {
-                        train.interval = parseInt($(this).val());
-                    }
-                });
+                    const train = {
+                        startingLocation: null,
+                        destination: null,
+                        firstTrainTime: null,
+                        trainInterval: 0
+                    };
 
-                showTrain(train);
+                    $.each($('input'), function () {
+                        if ($(this).is('input[name="train_name"]')) {
+                            train.startingLocation = $(this).val();
+                        } else if ($(this).is('input[name="train_destination"]')) {
+                            train.destination = $(this).val();
+                        } else if ($(this).is('input[name="first_train"]')) {
+                            train.firstTrainTime = $(this).val();
+                        } else if ($(this).is('input[name="departure_interval"]')) {
+                            train.trainInterval = parseInt($(this).val());
+                        }
+                    });
+
+                    // Update the Train Schedule table.
+                    addScheduledTrain(train);
+
+                    // Clear the form fields.
+                    form[0].reset();
+
+                    // Remove the validation trigger class.
+                    form.removeClass('was-validated');
+
+                    return false;
+                }
+            });
+
+            /**
+             * Handle the removal of a row from the Schedule table when
+             * the user click on the Trash/Remove button.
+             */
+            $('.table').on('click', '.remove', function () {
+                $(this).parents('tr').detach();
             });
         };
 
+        /**
+         * Initialize the application.
+         */
         const init = function () {
+            // Show the clock on the page.
             clock();
+
+            /**
+             * Configure the jQuery TimePicker extension to provide a
+             * uniform interface for defining the time for the first
+             * train across all browsers.
+             */
+            $('.timepicker').timepicker({
+                defaultTime: 'now',
+                dropdown: true,
+                dynamic: true,
+                interval: 15,
+                timeFormat: 'HH:mm'
+            });
+
+            // Register click and submit events.
             registerHandlers();
         };
 
+        /**
+         * Expose the public API for this module.
+         */
         return {
             start: init
         };
     })();
 
     TrainScheduler.start();
-
 })(jQuery);
